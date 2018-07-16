2a8de7275dd29327095ae4f5e427020efff3e41a
Chris Webster <cgw57@hotmail.com>
2018-07-16 15:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay(musicians,instruments) {
   var newArray = [];
   for (let i = 0; i < musicians.length; i++) {
-    
+    if (i === 0) {
+      return ("musicians[0] " + "instruments[0]");
+    }
   }
 }
